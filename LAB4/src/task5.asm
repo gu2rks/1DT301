d0074f88a77c8f00ef99f3ecabcdcceec6d6a9f7
gu2rks <aa224iu@student.lnu.se>
2018-10-07 17:54:53 +0200

    added task1-5 in lab4

--- LAB4/src/task5.asm
+++ LAB4/src/task5.asm
@@ -31,7 +31,7 @@
 .include "m2560def.inc"
 .def temp = r16
 .def char = r17
-.def flag = r18
+.def complement = r18
 .equ UBRR_val = 12					; osc. = 1MHz, 4800 bps
 
 .CSEG
@@ -40,10 +40,6 @@
 
 .org URXC1addr 
 	rjmp getChar
-
-.org UDRE1addr
-	rjmp putChar
-
 
 .org 0x72
 ;<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
@@ -58,53 +54,40 @@ start:
 	sts UBRR1L, temp
 
 	;TX = Transmitter, RX = receiver
-	ldi temp, (1<<RXEN1) | (1<<TXEN1) | (1<<RXCIE1) | (1<<UDRIE1)
+	ldi temp, (1<<RXEN1) | (1<<TXEN1) | (1<<RXCIE1)
 	sts UCSR1B, temp				; set TX and RX enable flags
 
 sei
 ;<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
 
 main:
-	call portOutput
+	nop
 rjmp main
 
 ;<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
 
 ;get char from PuTTY
 getChar:
-	
-	ldi flag, 1						; flag up
 	lds temp, UCSR1A 				; read UCSR1A I/O register to temp(r16)
 	sbrs temp, RXC1					; RXC1=1 => new char
 		rjmp getChar				; if temp clear = getChar
 	lds char, UDR1					; read char in UDR
 
-reti
-;<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
-
-portOutput:
-	
-	com char
-	out PORTB, char					; lights up LEDs
-	com char
-
-;<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+lightsChar:
+	mov complement, char			
+	com complement
+	out PORTB, complement
 
 ;echo char to PuTTY
 putChar:
-	
-	cpi flag, 0						; if flag = 0 means we already show the char in PuTTY
-	brne echo
-reti
-	
-;<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
-
-echo:
-
 	lds temp, UCSR1A				; read UCSR1A I/O register to temp(r16)
 	sbrs temp, UDRE1				; check if UDRE1 is empty
 		rjmp putChar				; if empty -> putchar
 	sts UDR1, char					; write char to UDR1
-	ldi flag, 0
-ret
+	rjmp getChar
+reti
+	
+;<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+
+
 
